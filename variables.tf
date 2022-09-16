9deaeb7a0406464ed51a2c531e443e63b3c5b65c
Dave Birkbeck <42197209+brickpack@users.noreply.github.com>
2022-09-15 18:31:39 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,55 +18,9 @@ variable "useRandomNamePart" {
 }
 
 # Redis parameters
-variable "sku" {
-  default     = "Enterprise_E10-2"
-  description = "https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/redis_enterprise_cluster#sku_name - e.g. Enterprise_E10-2"
-}
-
-## This should be enabled when the module supports an actual name
-# variable db_name {
-#   description = "The name which should be used for this Redis Enterprise Database. Changing this forces a new Redis Enterprise Database to be created."
-#   type        = string
-#   default     = "default" # As of now, "default" is the only value that's accepted
-# }
-
-variable "cluster_id" {
-  description = "* Changing this forces a new Redis Enterprise Database to be created. The resource id of the Redis Enterprise Cluster to deploy this Redis Enterprise Database."
-  type        = string
-  default     = ""
-}
-
-variable "zones" {
-  type        = list(string)
-  default     = [1, 2, 3]
-  description = "List of Availability Zones to deploy to. e.g. [1, 2, 3]"
-}
-
-variable "clustering_policy" {
-  type        = string
-  default     = "OSSCluster"
-  description = "* Changing this forces a new Redis Enterprise Database to be created. Clustering policy - default is OSSCluster. Specified at create time. Possible values are EnterpriseCluster and OSSCluster. Defaults to OSSCluster."
-}
-variable "client_protocol" {
-  type        = string
-  default     = "Encrypted"
-  description = "* Changing this forces a new Redis Enterprise Database to be created. Specifies whether redis clients can connect using TLS-encrypted or plaintext redis protocols. Default is TLS-encrypted. Possible values are Encrypted and Plaintext. Defaults to Encrypted."
-
-}
-variable "eviction_policy" {
-  type        = string
-  default     = "VolatileLRU"
-  description = "* Changing this forces a new Redis Enterprise Database to be created. Redis eviction policy - default is VolatileLRU. Possible values are AllKeysLFU, AllKeysLRU, AllKeysRandom, VolatileLRU, VolatileLFU, VolatileTTL, VolatileRandom and NoEviction. Defaults to VolatileLRU."
-}
-variable "port" {
-  type        = number
-  default     = 10000
-  description = "* Changing this forces a new Redis Enterprise Database to be created. TCP port of the database endpoint. Specified at create time. Defaults to an available port."
-}
-
 variable "locations" {
   type        = list
-  default     = []
+  default     = ["eastus2","westus2"]
   description = "(Optional) List of regions for Active Geo-Replication"
 }
 variable "linked_database_id" {
@@ -82,4 +36,48 @@ variable "linked_database_group_nickname" {
 variable "cluster_name_prefix" {
   type        = string
   description = "Prefix to use for the Azure Cache for Redis Enterprise cluster. Will be suffixed by the region name."
+}
+
+
+variable "sku" {
+  default     = "Enterprise_E10-2"
+  description = "https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/redis_enterprise_cluster#sku_name - e.g. Enterprise_E10-2"
+}
+## This should be enabled when the module supports an actual name
+# variable db_name {
+#   description = "The name which should be used for this Redis Enterprise Database. Changing this forces a new Redis Enterprise Database to be created."
+#   type        = string
+#   default     = "default" # As of now, "default" is the only value that's accepted
+# }
+
+variable "cluster_id" {
+  description = "* Changing this forces a new Redis Enterprise Database to be created. The resource id of the Redis Enterprise Cluster to deploy this Redis Enterprise Database."
+  type        = string
+  default     = ""
+}
+variable "zones" {
+  type        = list(string)
+  default     = [1, 2, 3]
+  description = "List of Availability Zones to deploy to. e.g. [1, 2, 3]"
+}
+
+variable "clustering_policy" {
+  type        = string
+  default     = "EnterpriseCluster"
+  description = "* Changing this forces a new Redis Enterprise Database to be created. Clustering policy - default is OSSCluster. Specified at create time. Possible values are EnterpriseCluster and OSSCluster. Defaults to OSSCluster."
+}
+variable "client_protocol" {
+  type        = string
+  default     = "Encrypted"
+  description = "* Changing this forces a new Redis Enterprise Database to be created. Specifies whether redis clients can connect using TLS-encrypted or plaintext redis protocols. Default is TLS-encrypted. Possible values are Encrypted and Plaintext. Defaults to Encrypted."
+}
+variable "eviction_policy" {
+  type        = string
+  default     = "VolatileLRU"
+  description = "* Changing this forces a new Redis Enterprise Database to be created. Redis eviction policy - default is VolatileLRU. Possible values are AllKeysLFU, AllKeysLRU, AllKeysRandom, VolatileLRU, VolatileLFU, VolatileTTL, VolatileRandom and NoEviction. Defaults to VolatileLRU."
+}
+variable "port" {
+  type        = number
+  default     = 10000
+  description = "* Changing this forces a new Redis Enterprise Database to be created. TCP port of the database endpoint. Specified at create time. Defaults to an available port."
 }
